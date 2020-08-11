b91dfb6144dda5dd60da82c6f8fa49c16fee59ff
BillyDavies1122 <36976243+BillyDavies1122@users.noreply.github.com>
2020-08-11 19:07:33 +0930

    Added todoitem METHODS

--- week3Tasks/Week3Tasks/Week3Tasks/Controllers/TodoController.cs
+++ week3Tasks/Week3Tasks/Week3Tasks/Controllers/TodoController.cs
@@ -7,7 +7,7 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/TodoController")]
     [ApiController]
     public class TodoController : ControllerBase
     {
@@ -25,6 +25,7 @@ namespace TodoApi.Controllers
                 _context.SaveChanges();
             }
         }
+
         // GET: api/Todo
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
@@ -45,5 +46,46 @@ namespace TodoApi.Controllers
 
             return todoItem;
         }
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
+
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
     }
 }
