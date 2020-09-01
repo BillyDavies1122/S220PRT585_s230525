2f02dd6277f405f4377cdce0884bd968c90a903f
BillyDavies1122 <36976243+BillyDavies1122@users.noreply.github.com>
2020-09-02 03:09:56 +0930

    Registered the context

--- Week6/ContosoUniversity/ContosoUniversity/Startup.cs
+++ Week6/ContosoUniversity/ContosoUniversity/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace ContosoUniversity
 {
@@ -24,6 +27,9 @@ namespace ContosoUniversity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SchoolContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
