48ec039c863d83252abe8b843bfbc80a9fc58281
BillyDavies1122 <36976243+BillyDavies1122@users.noreply.github.com>
2020-09-02 03:15:54 +0930

    Added test data and modified program

--- Week6/ContosoUniversity/ContosoUniversity/Startup.cs
+++ Week6/ContosoUniversity/ContosoUniversity/Startup.cs
@@ -30,6 +30,7 @@ namespace ContosoUniversity
 
             services.AddDbContext<SchoolContext>(options =>
         options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
